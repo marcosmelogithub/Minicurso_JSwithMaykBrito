39ceaa4694eef20cdce4e87b6feff08aa55d6218
Marcos Melo <marcosmelo@marcosmelo.com>
2024-03-02 19:35:09 -0300

    Declarando função com arrow function ao invés da palavra-chave function

--- functions.js
+++ functions.js
@@ -1,35 +1,10 @@
-// funções são linhas de códigos que executam ações pré-programadas
+// escrevendo arrow function
+// é uma outra forma de declarar uma função sem a necessidade de usar a palavra-chave function e funciona do mesmo jeito
+// um detalhe importante, é tentar sempre declarar a variável que for uma função com const para evitar que em algum ponto do JS esta variável tenha o seu conteúdo alterado e aí será perdida a função declarada para esta variável
 
-//===========================================================
-// Função Explícita
-//===========================================================
-// declarar uma função
-function CreatePhrases() {
-  console.log("Estudar é muito bom!")
-  console.log("Paciência e persistência!")
-  console.log("Revisão é mãe do aprendizado")
+// para declarar uma arrow function, basta substituir a palavra-chave function por => e colocar este => depois dos parentêses da função
+const MinhaArrowFunction = () => {
+  console.log("Escevendo a minha função arrow function");
 }
-
-// executar a função declarada
-CreatePhrases()
-
-//===========================================================
-// Função Expression ou Anonymous
-// quando a função é criada dentro de uma variável ou objeto
-//===========================================================
-const SumNumbers = function () {
-  return 10 + 20
-}
-// executar a constante SumNumbers, que é uma função, exibindo na console do DevTools
-console.log(SumNumbers())
-
-// passando parâmetros para a função
-// a função SumNumbersWithParameters receberá como parâmetros number1 e number2
-const SumNumbersWithParameters = function (number1, number2) {
-  return number1 + number2
-}
-// executar a constante SumNumbersWithParameters, exibindo na console do DevTools
-console.log(SumNumbersWithParameters(22, 40));
-// executar agora com interpolação - sempre usar crase ao invés de aspas ou apóstofre
-console.log(`O total da soma será ${SumNumbersWithParameters(60,100)}`);
-
+// executando a função
+MinhaArrowFunction();
