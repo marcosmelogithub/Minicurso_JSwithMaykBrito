9ecb34e5e30bb1ce207552571533610ac72162ea
Marcos Melo <marcosmelo@marcosmelo.com>
2024-03-02 18:13:45 -0300

    Entendendo Escopo (ou bloco), variáveis globais e locais

--- scripts.js
+++ scripts.js
@@ -1,11 +1,35 @@
-let testVariavel // apenas declarou a variável e não associou qq valor
-alert("Conteúdo da var: " + testVariavel + " tipo: " + typeof testVariavel) // a variável será do tipo undefined
+// Como estamos em um arquivo .js, o escopo geral é todo o arquivo, não necessitando
+// abrir e fechar com chaves "}"
 
-testVariavel = "texto" // altera o conteúdo da variável com um texto
-alert("Conteúdo da var: " + testVariavel + " tipo: " + typeof testVariavel) // agora a variável será do tipo string
+// declarar variável constante
+// exibir no console do DevTools (F12 do Browser)
+const varConstGlobal = "** Constante Global **"
+console.log("Exibindo uma constante global: ", varConstGlobal)
 
-testVariavel = true // altera o conteúdo da variável para true
-alert("Conteúdo da var: " + testVariavel + " tipo: " + typeof testVariavel) // agora a variável será do tipo boolean
+// declarar um escopo (ou bloco), abrindo com { e fechando com },
+// declarar variável com var dentro do escopo, será uma variável global
+// exibir no console do DevTools (F12 do Browser)
+{
+  var varGlobal = "** Variável Global **"
+  console.log(
+    "Exibindo variável global declarada dentro do escopo: ",
+    varGlobal
+  )
+}
 
-testVariavel = 45 // altera o conteúdo da variável para um número
-alert("Conteúdo da var: " + testVariavel + " tipo: " + typeof testVariavel) // agora a variável será do tipo number
+// alterar o conteúdo da variável global declarada dentro de um escopo
+// exibir variável global declarada dentro de um escopo no DevTools (F12 do Browser)
+varGlobal = "** valor alterado da variável global **"
+console.log("Exibindo alteração da variável global: ", varGlobal)
+
+// declarar outro escopo
+// criar variável local com let
+// exibir no console do DevTools (F12 do Browser)
+{
+  let varLocal = "** variável local **"
+  console.log("Exibindo variável local com let: ", varLocal)
+}
+
+// tentar exibir, fora do escopo, na console do DevTools (F12 do Browser) a variável local
+// ocorrerá erro porque a variável é local do escopo onde foi declarada
+console.log("Tentando exibir variável local de um escopo: ", varLocal)
