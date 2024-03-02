90dd8763ecc4b5cd21f29332238e627a28a0c6bb
Marcos Melo <marcosmelo@marcosmelo.com>
2024-03-02 15:25:43 -0300

    Declaração de variáveis no JS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-// exibe uma lista (array) com dois elementos
+// usando var para declarar variável
+var verao = ""; // criar variável e iniciada com vazio
+verao = "quente"; // atribuir valor a var1
+alert("verão é " + verao); // exibir conteúdo de var1 com alert
+
+// usando let para declarar variável
+let inverno = ""; // criar variável e iniciada com vazio
+inverno = "frio"; // atribuir valor a var1
+alert("inverno é " + inverno); // exibir conteúdo de var1 com alert
+
+// usando const para declarar variável
+const primavera = "Primavera é a estação das flores";
+alert(primavera);
