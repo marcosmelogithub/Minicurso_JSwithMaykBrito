60dd3856adfd7a581fb18b1fbf45e9b13ce8edf8
Marcos Melo <marcosmelo@marcosmelo.com>
2024-03-02 20:26:25 -0300

    Entendendo e aplicando Function Constructor ou Função Construtora

--- functions.js
+++ functions.js
@@ -1,26 +1,32 @@
-// callback function
-// é simplesmente quando se passa uma função como parâmetro para outra função executar em um determinado ponto da sua execução
+// Function Constuctor ou Função Construtora
+// é um recurso para se criar objetos com propriedades e funcionalidades iguais, porém com atributos ou até mesmo métodos diferentes
 
-// declarando uma função de forma padrão
-function FuncaoMaster(funcaoParam) {
-  console.log('antes de executar a funcão recebida como parametro');
-  funcaoParam();
-  console.log('depois que executou a função recebida como parametro');
-}
-// executando a FuncaoMaster passando uma função como parametro
-FuncaoMaster(() => {console.log('executando a função passada como parametro')});
-
-
-console.log('=======================================');
-console.log('INSTRUÇÃO: de uma forma diferente, declarar a função antes e passar como parametro');
-console.log("=======================================")
-
-
-// numa forma diferente, declara-se primeiro a função que será passda como parametro
-function FuncaoPassadaComoParametro() {
-  console.log('executando função passada como parametro, porém declarada antes');
+// declarar um função construtora
+function Person(nameParam){
+  this.name = nameParam;  // criou-se uma propriedade chama name com o conteúdo do parametro nameParam recebido
 }
 
-// quando passar uma função como parametro que foi declarada, não usar os parenteses, passar somente o nome da função
-FuncaoMaster(FuncaoPassadaComoParametro);
+// declarando variável que irá conter o objeto construído pela função Person
+const personNumber1 = new Person('Marcos Melo');
+console.log(personNumber1);   // retorna o objeto criado
+console.log(personNumber1.name) // retorna o atributo da propriedade name do objeto
 
+// declarando outra variável usando a função construtora Person com outro atributo
+const personNumber2 = new Person('Aurora Melo');
+console.log(personNumber2);
+console.log(personNumber2.name);
+
+// pode-se assim criar objetos inteiros alterando os atributos das propriedades e métodos das funcionalidades, criando uma forma dinâmica de tratar os dados.
+// o nome das funções construtoras, como uma regra tácita, sempre começam com letra maiúscula, apesar que se escrever começando com letra minúscula, ou qq caracter válido, também funciona.
+
+
+// exemplo de funções construtoras pré-existentes no JS
+
+// função construtora: String
+const myName = new String('Marcos Melo'); // montará o objeto String{'Marcos Melo'}
+console.log(myName); // exibirá o objeto criado
+console.log(myName[0]); // por ser string, pode-se acessar cada caracter como array
+
+// função construtora: Date
+const actualDate = new Date(); // cria objeto com a data e hora atuais
+console.log(actualDate); // exibe a data na console do DevTools
